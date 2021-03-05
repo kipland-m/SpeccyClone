01ebd099f47b05d5f94c7737cbd82a7b629205a7
Kipland Melton <kipmelton@yahoo.com>
2021-03-04 23:40:27 -0600

    test

--- memory_info.py
+++ memory_info.py
@@ -1,8 +1,21 @@
 # Kipland Melton
 import psutil
 
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
 def RetrieveMemory():
 
+    ram_info = psutil.virtual_memory()
+
+    for info in ram_info:
+        print(info)
 
 if __name__ == "__main__":
     RetrieveMemory()
