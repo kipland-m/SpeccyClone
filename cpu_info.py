b2ffd77513f5b268cc2ab04698799c3911e4b0e4
Kipland Melton <kipmelton@yahoo.com>
2022-02-17 16:21:27 -0600

    successfully display cpu % used

--- cpu_info.py
+++ cpu_info.py
@@ -4,9 +4,15 @@ import psutil
 def RetrieveCPU():
 
     core_Freq = psutil.cpu_freq()
+
+    # Ideas for info fields
+    # CPU Name (i.e. i7-9700k)
+    # CPU Temp?
+    # CPU usage
     
     print('Base clock speed : {} MHz'.format(core_Freq[0]))
     print('Core count : {}'.format(psutil.cpu_count()))
+    print('Percent used : {}'.format(psutil.cpu_percent(interval=1)))
   
     
 if __name__ == "__main__":
