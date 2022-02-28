5e598a598db670cecc60c5bd8d098d31e37a41ea
Kipland Melton <kipmelton@yahoo.com>
2022-02-27 20:52:47 -0600

    Added processor import, getting CPU name

--- cpu_info.py
+++ cpu_info.py
@@ -1,5 +1,5 @@
 # Kipland Melton
-import psutil
+import psutil, platform
 
 def RetrieveCPU():
 
@@ -10,9 +10,10 @@ def RetrieveCPU():
     # CPU Temp?
     # CPU usage
     
-    print('Base clock speed : {} MHz'.format(core_Freq[0]))
+    print('Base clock speed : {} MHz'.format(psutil.cpu_freq(percpu=True))) # Realtime values only support on linux for cpu_freq
     print('Core count : {}'.format(psutil.cpu_count()))
     print('Percent used : {}'.format(psutil.cpu_percent(interval=1)))
+    print(platform.processor())
   
     
 if __name__ == "__main__":
