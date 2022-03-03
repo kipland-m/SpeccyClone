d7ffdcabe585c526f961e6186222c2b5900c3757
Kipland Melton <kipmelton@yahoo.com>
2022-03-02 21:15:18 -0600

    added notes to cpu_info

--- cpu_info.py
+++ cpu_info.py
@@ -4,12 +4,15 @@ import psutil, platform
 def RetrieveCPU():
 
     core_Freq = psutil.cpu_freq()
+    
 
     # Ideas for info fields
     # CPU Name (i.e. i7-9700k)
     # CPU Temp?
     # CPU usage
-    
+    # CPU Core Count (Virtual and physical)
+    # CPU Clock speed
+
     print('Base clock speed : {} MHz'.format(psutil.cpu_freq(percpu=True))) # Realtime values only support on linux for cpu_freq
     print('Core count : {}'.format(psutil.cpu_count()))
     print('Percent used : {}'.format(psutil.cpu_percent(interval=1)))
