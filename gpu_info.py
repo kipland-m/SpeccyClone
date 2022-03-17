7663ca3a0156233fd57f49c5aa9cc1272fe461bc
Michael Ashby <msashby00@gmail.com>
2022-03-16 22:58:12 -0400

    Created tuple of parameter variables, For loop to print both parameter and label tuples, and created GPUtil object that needs call class parameters.

--- gpu_info.py
+++ gpu_info.py
@@ -1,17 +1,18 @@
-# A method to retrieve GPU statistics
-#
-# gpu_info method
+# Used retrieve GPU statistics
+# GPUtil only works on Nvidia cards due to its reliance on nvidia-smi utility.
+# Had to create a folder called NVSMI under C:\Program Files\NVIDIA Corporation, and
+# I had to add these files nvidia-smi.exe and nvml.dll files under C:\Windows\System32 to the NVSMI folder.
 #
 # Michael Ashby
 # Start:    3/5/2021
-# Updated:  2/16/2022
+# Updated:  3/16/2022
 
 import psutil
 import GPUtil
 import math
 
 
-#Declaring and initializating string variables holding gpu statistics.
+# Declaring and initializating string variables holding gpu statistics.
 gpuName = None
 driverVerison = None
 driverDate = None
@@ -19,29 +20,38 @@ gpuTemperature = None
 gpuUtilization = None
 gpuSharedMemory = None
 gpuDedicatedMemory = None
-gpuMainHead = None
+gpuObjs = []
 
+# Holds number of requirements to display.
 StatisticCount = 7
 
-gpuMainHead = GPUtil.getGPUs()
-gpuName = gpuMainHead.name
-
 # Initialized an array of Strings holding the labels for the GPU data pulled. 
-Column1Tuple = ("GPU Name:  ", "Driver Verison:  ", "Driver Date:  ", "GPU Temperature:  ", "GPU Utilization:  ", "GPU Shared Memory:  ", "GPU Dedicated Memory:  ")
+LabelTuple = ("GPU Name:  ", "Driver Verison:  ", "Driver Date:  ", "GPU Temperature:  ", "GPU Utilization:  ", "GPU Shared Memory:  ", "GPU Dedicated Memory:  ")
 
-# Initialized an array of Strings holding the data for the GPU.
-Column2Tuple = ()
+# Initialized an array of Strings holding the variables for the GPU.
+VariableTuple = (gpuName, driverVerison, driverDate, gpuTemperature, gpuUtilization, gpuSharedMemory, gpuDedicatedMemory)
 
-#Try printing left and right column, catch an Exception for debugging.  
 try:
-    #Need functions returning values for each variable initialized in lines 14-20
+  gpuObjs = GPUtil.getGPUs()
+except Exception:
+  print("Failed to retrieve GPUs.")
 
-    #Loop through the tuple and print its corresponding string
+# Working on pulling parameters from the GPUtil object.
+print (gpuObjs)
+
+for gpuObj in gpuObjs:
+  gpuName = gpuObj.name
+  driverVerison = gpuObj.driver
+  # driverDate = gpuObj.
+  # gpuTemperature = gpuData.
+  gpuUtilization = gpuObj.load
+  # gpuSharedMemory = gpuObj.
+  # gpuDedicatedMemory = 
+
+# Try printing left and right column, catch an Exception for debugging.  
+try:
     for y in range(StatisticCount):
-      
-      #Print the correlating Drive Name
-      print(Column1Tuple[y])
-      #print(Column2Tuple[y])
-        
+      print(LabelTuple[y])
+      print(VariableTuple[y])    
 except FileNotFoundError:
     print("Error parsing and printing GPU information")
